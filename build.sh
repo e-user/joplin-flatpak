6ec7f17db4583242155a6a5d1394343e7b165c16
Jete O'Keeffe <jeteokeeffe@yahoo.com>
2020-04-17 17:29:04 -0400

    update version of joplin app

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+FILE="com.github.laurent22.joplin.json"
+BUILD_DIR="flatpak-build"
+
+STOP_MODULE="joplin"
+STOP_AT=""
 
 #flatpak-builder --install-deps-from=flathub --install --user --force-clean flatpak-build com.github.laurent22.joplin.json
-flatpak-builder --install-deps-from=flathub --install --force-clean flatpak-build com.github.laurent22.joplin.json
+flatpak-builder --install-deps-from=flathub --install --force-clean $STOP_AT $BUILD_DIR $FILE
